9e03e5f01f22d4edb42780920abdedcd2dfab4e9
hunter wu <hunterfront@163.com>
2018-04-26 18:22:28 +0800

    fix backtotop

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,19 +1,17 @@
 <template>
-  <div>
-    <header id="hd" :class="{open: isOpen}">
-      <div class="page-cover" v-show="isOpen" @click="closeMenu"></div>
-      <div class="nv-bar">
-        <div class="nv-bar__menu-btn" @click="openMenu"></div>
-        <span class="nv-bar__txt">{{headTxt}}</span>
-        <div class="nv-bar__add">
-          <router-link to="/add">
-            <i class="iconfont add-icon">&#xe60f;</i>
-          </router-link>
-        </div>
+  <header id="hd" :class="{open: isOpen}">
+    <div class="page-cover" v-show="isOpen" @click="closeMenu"></div>
+    <div class="nv-bar">
+      <div class="nv-bar__menu-btn" @click="openMenu"></div>
+      <span class="nv-bar__txt">{{headTxt}}</span>
+      <div class="nv-bar__add">
+        <router-link to="/add">
+          <i class="iconfont add-icon">&#xe60f;</i>
+        </router-link>
       </div>
-      <nvMenu></nvMenu>
-    </header>
-  </div>
+    </div>
+    <nvMenu></nvMenu>
+  </header>
 </template>
 
 <script>
